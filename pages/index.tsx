328a87f443c901fc039ece7cadddb5b482a2b5b9
MAliAKbar22 <maakbarofficial@gmail.com>
2023-05-04 10:00:39 +0500

    error changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ import { fetchProjects } from "../utils/fetchProjects";
 import { fetchSkills } from "../utils/fetchSkills";
 import { fetchSocials } from "../utils/fetchSocials";
 import Head from "next/head";
+import Image from "next/image";
 
 type Props = {
   pageInfo: PageInfo;
@@ -39,7 +40,7 @@ export default function Home({
       <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
         <Header socials={socials} />
         <section id="hero" className="snap-start">
-          <Hero pageInfo={pageInfo} />
+          <Hero pageinfo={pageInfo} />
         </section>
         <section id="about" className="snap-center">
           <About pageInfo={pageInfo} />
@@ -59,10 +60,10 @@ export default function Home({
         <Link href="#hero">
           <footer className="sticky bottom-5 w-full cursor-pointer">
             <div className="flex items-center justify-center">
-              <img
+              <Image
                 className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
                 src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/sites/160477/images/F69olKlTDWfJQcAnS4jw_file.jpg"
-                alt=""
+                alt="Scroll-To-Top"
               />
             </div>
           </footer>
